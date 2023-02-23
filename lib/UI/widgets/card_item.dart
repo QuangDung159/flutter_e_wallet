b5d391f84a485f29d6f782e43b8d176f98e8980c
QDLu <quangdunglu159@gmail.com>
2023-02-23 22:16:03 +0700

    Wallet screen

---
+++ lib/UI/widgets/card_item.dart
@@ -0,0 +1,94 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+
+import 'package:flutter/material.dart';
+import 'package:flutter_e_wallet/core/constants/app_colors.dart';
+import 'package:flutter_e_wallet/core/constants/app_dimension.dart';
+import 'package:flutter_e_wallet/core/helpers/asset_helper.dart';
+
+class CardItem extends StatelessWidget {
+  const CardItem({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        Container(
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(12),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.grey.withOpacity(0.2),
+                blurRadius: 4,
+              ),
+            ],
+          ),
+          margin: EdgeInsets.only(
+            bottom: AppDimension.contentPadding,
+          ),
+          padding: EdgeInsets.symmetric(
+            horizontal: AppDimension.contentPadding,
+          ),
+          child: Row(
+            children: [
+              SizedBox(
+                width: 10,
+              ),
+              Image.asset(
+                AssetHelper.iconMastercard,
+                width: 80,
+                height: 80,
+              ),
+              SizedBox(
+                width: 10,
+              ),
+              Expanded(
+                child: Column(
+                  crossAxisAlignment:
+                      CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      '**** **** **** 4242',
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Text(
+                      'Visa',
+                      style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.w900),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(
+                width: 10,
+              ),
+            ],
+          ),
+        ),
+        Positioned(
+          right: 16,
+          top: 16,
+          child: Container(
+            width: 20,
+            height: 20,
+            decoration: BoxDecoration(
+              color: AppColors.bgPrimary,
+              borderRadius: BorderRadius.circular(10),
+            ),
+            child: Icon(
+              Icons.close_rounded,
+              color: Colors.white,
+              size: 16,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
