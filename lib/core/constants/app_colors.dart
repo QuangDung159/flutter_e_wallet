95a52601e125caeb7426660fae64a595a2855c69
QDLu <quangdunglu159@gmail.com>
2023-02-23 20:19:28 +0700

    Setup libs

---
+++ lib/core/constants/app_colors.dart
@@ -0,0 +1,11 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static final Color primaryBlue = Color(0xff404CB3);
+  static final Color textDefault = Color(0xff3D6670);
+  static final Color bgSeparator = Color(0xffDDDDDD);
+  static final Color textDisable = Color(0xff94AFB6);
+  static final Color bgPrimary = Color(0xff243972);
+}
